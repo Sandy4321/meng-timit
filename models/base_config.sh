7aa4c173c5f55b6a29704fe9a0bd667a0d013ab8
Drew Titus <atitus@mit.edu>
2018-04-02 08:05:55 -0400

    Added smaller LR results; trying even smaller LR

--- models/base_config.sh
+++ models/base_config.sh
@@ -2,7 +2,7 @@ export FEAT_DIM=40      # 40-dim Mel filter bank
 export LEFT_CONTEXT=7
 export RIGHT_CONTEXT=7
 export OPTIMIZER=Adam
-export LEARNING_RATE=0.001
+export LEARNING_RATE=0.00001
 export EPOCHS=35
 export BATCH_SIZE=128
 
@@ -10,7 +10,7 @@ export CHANNELS=( 64 128 128 )
 export KERNELS=( 5 3 3 )        # Assume square kernels (AxA)
 export DOWNSAMPLES=( 2 0 2 )          # Pool only in frequency; no overlap. Use 0 to indicate no pooling
 
-export LATENT_DIM=1024
+export LATENT_DIM=512
 
 export USE_BATCH_NORM=false
 
@@ -18,7 +18,7 @@ export CHANNELS_DELIM=$(printf "_%s" "${CHANNELS[@]}")
 export KERNELS_DELIM=$(printf "_%s" "${KERNELS[@]}")
 export DOWNSAMPLES_DELIM=$(printf "_%s" "${DOWNSAMPLES[@]}")
 
-export PHONE_FC=( 1024 1024 )
+export PHONE_FC=( 512 512 )
 export PHONE_FC_DELIM=$(printf "_%s" "${PHONE_FC[@]}")
 
 export CLEAN_RECIPE=${RECIPES}/100_rir_baseline_clean
