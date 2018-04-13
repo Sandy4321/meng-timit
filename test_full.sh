29a816c43b13b0f8af6aa80f9cf8039b1d2a222c
Drew Titus <atitus@mit.edu>
2018-04-13 19:04:50 -0400

    Added a few more results

--- test_full.sh
+++ test_full.sh
@@ -19,15 +19,16 @@ echo $dirty_train_job_output
 dirty_train_job_id=$( python3 -c "print(\"$dirty_train_job_output\".split(\" \")[-1])" )
 sbatch --dependency=afterany:$dirty_train_job_id models/eval_acoustic_model.bash dirty
 
-# Train models that are dependent on the baseline
+# Train models that are dependent on the baseline for evaluation
 model_types=( enhancement_net enhancement_md )
 for model_type in "${model_types[@]}"; do
     # Since sbatch now prints "Submitted batch job <X>", need to get last element in
     # space-delimited array to get the actual Job ID
-    train_job_output=$(sbatch --dependency=afterany:$clean_eval_job_id models/train_${model_type}.bash)
+    train_job_output=$(sbatch models/train_${model_type}.bash)
     echo $train_job_output
     train_job_id=$( python3 -c "print(\"$train_job_output\".split(\" \")[-1])" )
-    sbatch --dependency=afterany:$train_job_id models/eval_${model_type}.bash
+    sbatch --dependency=afterany:$train_job_id models/histogram_${model_type}.bash
+    sbatch --dependency=afterany:$train_job_id:$clean_eval_job_id models/eval_${model_type}.bash
 done
 
 # Train models that aren't dependent on the baseline
@@ -39,4 +40,8 @@ for model_type in "${model_types[@]}"; do
     echo $train_job_output
     train_job_id=$( python3 -c "print(\"$train_job_output\".split(\" \")[-1])" )
     sbatch --dependency=afterany:$train_job_id models/eval_${model_type}.bash
+    
+    if [ "$model_type" != "end2end_phone_net" ]; then
+        sbatch --dependency=afterany:$train_job_id models/histogram_${model_type}.bash
+    fi
 done
